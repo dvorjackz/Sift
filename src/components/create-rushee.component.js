c29f4b82e7bfac1431634142499befd09bf45cae
Jack Zhang <hijackz337@gmail.com>
2020-01-06 23:18:12 -0800

    Add navbar

---
+++ src/components/create-rushee.component.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+
+export default class CreateRushee extends Component {
+
+    render() {
+        return (
+            <div>
+                <p>Add a Rushee.</p>
+            </div>
+        );
+    }
+
+}
---
+++ src/components/rankings.component.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+
+export default class Rankings extends Component {
+
+    render() {
+        return (
+            <div>
+                <p>Rankings.</p>
+            </div>
+        );
+    }
+
+}
