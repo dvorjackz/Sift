ab8c46ef2e2450c94f9e119b85c11ca1a1f12662
Jack Zhang <hijackz337@gmail.com>
2020-01-14 12:31:15 -0800

    Resume file upload added

--- src/components/create-rushee.component.js
+++ src/components/create-rushee.component.js
@@ -1,12 +1,38 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import axios from 'axios';
 
 export default class CreateRushee extends Component {
+
+    state = {
+        selectedFile: null
+    }
+
+    fileSelectedHandler = event => {
+        console.log(event.target.files[0]);
+        this.setState({
+            selectedFile: event.target.files[0]
+        });
+    }
+
+    fileUploadHandler = event => {
+        const fd = new FormData();
+        fd.append('image', this.state.selectedFile, this.state.selectedFile.name);
+        axios.post('http://localhost:5000/rushees/upload-resume', fd, {
+            onUploadProgress: progressEvent => {
+                console.log("Upload progress: " + Math.round((progressEvent.loaded / progressEvent.total) * 100) + "%");
+            }
+        }).then((res) => {
+            console.log(res);
+        });
+    }
 
     render() {
         return (
             <div>
                 <p>Add a Rushee.</p>
+                <input type="file" onChange={this.fileSelectedHandler} />
+                <button onClick={this.fileUploadHandler}>Upload</button>
             </div>
         );
     }
