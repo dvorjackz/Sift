7cb6b3072a7378bb7f2417603e64dcde5f3843af
Jack Zhang <hijackz337@gmail.com>
2020-01-14 17:03:14 -0800

    Add show pdf display functionality

--- src/components/sift.component.js
+++ src/components/sift.component.js
@@ -1,12 +1,16 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import { PDFReader } from 'react-read-pdf'
 
 export default class Sift extends Component {
 
     render() {
         return (
             <div>
-                <p>Sift.</p>
+                <p>Sift!</p> 
+
+                <embed src="https://drive.google.com/viewerng/viewer?embedded=true&url=https://dsp-winter-2020-resumes.s3-us-west-1.amazonaws.com/JackZhang-Resume.pdf" width="500" height="375" />
+
             </div>
         );
     }
